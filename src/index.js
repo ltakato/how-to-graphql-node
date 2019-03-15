5863827cc42e2809925fcf015fdfd3d4e7bd7f2e
takato <leonardo.takato@soaexpert.com.br>
2019-03-15 09:51:05 -0300

    feat: add Link feed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 };
 
